78b3ebc75c178f33f58281e7f5e8e6fd846798a1
Ekuremaro <86929331+Ekuremaro@users.noreply.github.com>
2022-05-15 11:48:44 +0100

    added private route

--- src/App.js
+++ src/App.js
@@ -10,24 +10,29 @@ import {
   About,
   Products,
   PrivateRoute,
+  AuthWrapper,
 } from './pages';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Sidebar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/cart" component={Cart} />
-        <Route exact path="/checkout" component={Checkout} />
-        <Route exact path="/products" component={Products} />
-        <Route exact path="/products/:id" children={<SingleProduct />} />
-        <Route exact path="*" component={Error} />
-      </Switch>
-      <Footer />
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/cart" component={Cart} />
+          <PrivateRoute exact path="/checkout">
+            <Checkout />
+          </PrivateRoute>
+          <Route exact path="/products" component={Products} />
+          <Route exact path="/products/:id" children={<SingleProduct />} />
+          <Route exact path="*" component={Error} />
+        </Switch>
+        <Footer />
+      </Router>
+    </AuthWrapper>
   );
 }
 
